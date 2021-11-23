a54a8a50e534c800c59aeb774c31562d8e847d42
fcx <fcx@pingxingyun.com>
2021-11-23 15:45:23 +0800

    jni入口函数

--- xr_app_huawei/src/main/cpp/main.cpp
+++ xr_app_huawei/src/main/cpp/main.cpp
@@ -3,4 +3,11 @@
 //
 
 #include "main.h"
+#include <android/window.h>				// for AWINDOW_FLAG_KEEP_SCREEN_ON
+#include <android/native_window_jni.h>	// for native window JNI
+#include <android_native_app_glue.h>
 
+// JNI 入口函数必须定义
+void android_main( struct android_app * app ) {
+
+}
