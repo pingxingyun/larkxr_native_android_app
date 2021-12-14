6e2c8692184cf6cf279a083833dc38b08fa6d638
Hayasu-Yumito <609029250@qq.com>
2021-12-14 09:55:04 +0800

    新增华为ar集成demo

---
+++ ar_huawei_app/src/main/java/com/pxy/ar_huawei_app/MainActivity.java
@@ -0,0 +1,256 @@
+package com.pxy.ar_huawei_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.huawei.hiar.ARConfigBase;
+import com.huawei.hiar.AREnginesApk;
+import com.huawei.hiar.ARSession;
+import com.huawei.hiar.ARWorldTrackingConfig;
+import com.huawei.hiar.exceptions.ARCameraNotAvailableException;
+import com.huawei.hiar.exceptions.ARUnSupportedConfigurationException;
+import com.huawei.hiar.exceptions.ARUnavailableClientSdkTooOldException;
+import com.huawei.hiar.exceptions.ARUnavailableServiceApkTooOldException;
+import com.huawei.hiar.exceptions.ARUnavailableServiceNotInstalledException;
+import com.pxy.ar_huawei_app.rendering.WorldRenderManager;
+import com.pxy.ar_huawei_app.util.DisplayRotationManager;
+import com.pxy.ar_huawei_app.util.GestureEvent;
+import com.pxy.ar_huawei_app.util.LogUtil;
+import com.pxy.ar_huawei_app.util.PermissionManager;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class MainActivity extends Activity {
+    private static final String TAG =MainActivity.class.getSimpleName();
+
+    private static final int MOTIONEVENT_QUEUE_CAPACITY = 2;
+
+    private static final int OPENGLES_VERSION = 2;
+
+    private ARSession mArSession;
+
+    private GLSurfaceView mSurfaceView;
+
+    private WorldRenderManager mWorldRenderManager;
+
+    private GestureDetector mGestureDetector;
+
+    private DisplayRotationManager mDisplayRotationManager;
+
+    private ArrayBlockingQueue<GestureEvent> mQueuedSingleTaps = new ArrayBlockingQueue<>(MOTIONEVENT_QUEUE_CAPACITY);
+
+    private String message = null;
+
+    private boolean isRemindInstall = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PermissionManager.checkPermission(this);
+
+        FWBID();
+        Init();
+    }
+    private void FWBID() {
+        mSurfaceView = findViewById(R.id.surfaceview);
+    }
+
+    private void Init() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        mDisplayRotationManager = new DisplayRotationManager(this);
+        initGestureDetector();
+
+        mSurfaceView.setPreserveEGLContextOnPause(true);
+        mSurfaceView.setEGLContextClientVersion(OPENGLES_VERSION);
+
+        // Set the EGL configuration chooser, including for the number of
+        // bits of the color buffer and the number of depth bits.
+        mSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+
+        mWorldRenderManager = new WorldRenderManager(this, this);
+        mWorldRenderManager.setDisplayRotationManage(mDisplayRotationManager);
+        mWorldRenderManager.setQueuedSingleTaps(mQueuedSingleTaps);
+
+        mSurfaceView.setRenderer(mWorldRenderManager);
+        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    private void initGestureDetector() {
+        mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent motionEvent) {
+                onGestureEvent(GestureEvent.createDoubleTapEvent(motionEvent));
+                return true;
+            }
+
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+                onGestureEvent(GestureEvent.createSingleTapConfirmEvent(motionEvent));
+                return true;
+            }
+
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                onGestureEvent(GestureEvent.createScrollEvent(e1, e2, distanceX, distanceY));
+                return true;
+            }
+        });
+
+        mSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return mGestureDetector.onTouchEvent(event);
+            }
+        });
+    }
+
+    private void onGestureEvent(GestureEvent e) {
+        boolean offerResult = mQueuedSingleTaps.offer(e);
+        if (offerResult) {
+            LogUtil.debug(TAG, "Successfully joined the queue.");
+        } else {
+            LogUtil.debug(TAG, "Failed to join queue.");
+        }
+    }
+
+    private void setMessageWhenError(Exception catchException) {
+        if (catchException instanceof ARUnavailableServiceNotInstalledException) {
+            startActivity(new Intent(this,ConnectAppMarketActivity.class));
+        } else if (catchException instanceof ARUnavailableServiceApkTooOldException) {
+            message = "Please update HuaweiARService.apk";
+        } else if (catchException instanceof ARUnavailableClientSdkTooOldException) {
+            message = "Please update this app";
+        } else if (catchException instanceof ARUnSupportedConfigurationException) {
+            message = "The configuration is not supported by the device!";
+        } else {
+            message = "exception throw";
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        LogUtil.debug(TAG, "onResume");
+        super.onResume();
+        if (!PermissionManager.hasPermission(this)) {
+            this.finish();
+        }
+        message = null;
+        if (mArSession == null) {
+            try {
+                if (!arEngineAbilityCheck()) {
+                    finish();
+                    return;
+                }
+                mArSession = new ARSession(this);
+                ARWorldTrackingConfig config = new ARWorldTrackingConfig(mArSession);
+                config.setFocusMode(ARConfigBase.FocusMode.AUTO_FOCUS);
+                config.setSemanticMode(ARWorldTrackingConfig.SEMANTIC_PLANE);
+                mArSession.configure(config);
+                mWorldRenderManager.setArSession(mArSession);
+            } catch (Exception capturedException) {
+                setMessageWhenError(capturedException);
+            }
+            if (message != null) {
+                stopArSession();
+                return;
+            }
+        }
+        try {
+            mArSession.resume();
+        } catch (ARCameraNotAvailableException e) {
+            Toast.makeText(this, "Camera open failed, please restart the app", Toast.LENGTH_LONG).show();
+            mArSession = null;
+            return;
+        }
+        mDisplayRotationManager.registerDisplayListener();
+        mSurfaceView.onResume();
+    }
+
+    /**
+     * Check whether HUAWEI AR Engine server (com.huawei.arengine.service) is installed on the current device.
+     * If not, redirect the user to HUAWEI AppGallery for installation.
+     *
+     * @return true:AR Engine ready
+     */
+    private boolean arEngineAbilityCheck() {
+        boolean isInstallArEngineApk = AREnginesApk.isAREngineApkReady(this);
+        if (!isInstallArEngineApk && isRemindInstall) {
+            Toast.makeText(this, "Please agree to install.", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        if (!isInstallArEngineApk) {
+            startActivity(new Intent(this,ConnectAppMarketActivity.class));
+            isRemindInstall = true;
+        }
+        return AREnginesApk.isAREngineApkReady(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!PermissionManager.hasPermission(this)) {
+            Toast.makeText(this, "This application needs camera permission.", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    private void stopArSession() {
+        LogUtil.info(TAG, "stopArSession start.");
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        if (mArSession != null) {
+            mArSession.stop();
+            mArSession = null;
+        }
+        LogUtil.info(TAG, "stopArSession end.");
+    }
+
+    @Override
+    protected void onPause() {
+        LogUtil.info(TAG, "onPause start.");
+        super.onPause();
+        if (mArSession != null) {
+            mDisplayRotationManager.unregisterDisplayListener();
+            mSurfaceView.onPause();
+            mArSession.pause();
+        }
+        LogUtil.info(TAG, "onPause end.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        LogUtil.info(TAG, "onDestroy start.");
+        if (mArSession != null) {
+            mArSession.stop();
+            mArSession = null;
+        }
+        super.onDestroy();
+        LogUtil.info(TAG, "onDestroy end.");
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean isHasFocus) {
+        LogUtil.debug(TAG, "onWindowFocusChanged");
+        super.onWindowFocusChanged(isHasFocus);
+        if (isHasFocus) {
+            getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+}
---
+++ ar_huawei_app/src/main/java/com/pxy/ar_huawei_app/util/LogUtil.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2021. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.pxy.ar_huawei_app.util;
+
+import android.util.Log;
+
+/**
+ * Log utility class, which provides a more convenient method of printing logs.
+ *
+ * @author HW
+ * @since 2021-03-08
+ */
+public class LogUtil {
+    private static final String PROJECT_NAME = "ArEngine_demo";
+
+    /**
+     * Method name.
+     */
+    private static String methodName;
+
+    /**
+     * Line number.
+     */
+    private static int lineNumber;
+
+    /**
+     * Create log information.
+     *
+     * @param tag Tag.
+     * @param logMsg Log messages.
+     * @return Packaged log messages.
+     */
+    private static String createLog(String tag, String logMsg) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("[").append(tag);
+        buffer.append(": ").append(methodName);
+        buffer.append(": ").append(lineNumber).append("] ");
+        buffer.append(logMsg);
+        return buffer.toString();
+    }
+
+    /**
+     * Obtain the method name and line number in the printed log.
+     *
+     * @param stackElements Stack elements.
+     */
+    private static void getMethodNames(StackTraceElement[] stackElements) {
+        methodName = stackElements[1].getMethodName();
+        lineNumber = stackElements[1].getLineNumber();
+    }
+
+    /**
+     * Print verbose logs.
+     *
+     * @param tag Tag.
+     * @param message Log messages.
+     */
+    public static void verbose(String tag, String message) {
+        getMethodNames(new Throwable().getStackTrace());
+        Log.v(PROJECT_NAME, createLog(tag, message));
+    }
+
+    /**
+     * Print debug logs.
+     *
+     * @param tag Tag.
+     * @param message Log messages.
+     */
+    public static void debug(String tag, String message) {
+        getMethodNames(new Throwable().getStackTrace());
+        Log.d(PROJECT_NAME, createLog(tag, message));
+    }
+
+    /**
+     * Print info logs.
+     *
+     * @param tag Tag.
+     * @param message Log messages.
+     */
+    public static void info(String tag, String message) {
+        getMethodNames(new Throwable().getStackTrace());
+        Log.i(PROJECT_NAME, createLog(tag, message));
+    }
+
+    /**
+     * Print warn logs.
+     *
+     * @param tag Tag.
+     * @param message Log messages.
+     */
+    public static void warn(String tag, String message) {
+        getMethodNames(new Throwable().getStackTrace());
+        Log.w(PROJECT_NAME, createLog(tag, message));
+    }
+
+    /**
+     * Print error logs.
+     *
+     * @param tag Tag.
+     * @param message Log messages.
+     */
+    public static void error(String tag, String message) {
+        getMethodNames(new Throwable().getStackTrace());
+        Log.e(PROJECT_NAME, createLog(tag, message));
+    }
+}
